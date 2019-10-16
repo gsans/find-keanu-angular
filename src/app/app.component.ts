6e665c4e528b8fbc1ae7f9fc05497b522d45eda8
Gerard Sans <gsans@amazon.com>
2019-10-16 23:13:37 +0100

    Initial commit 😎

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import Predictions from '@aws-amplify/predictions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'amplify-app-ml-test';
+export class AppComponent implements OnInit {
+  public createForm: FormGroup;
+  resultMessage = "Pick a selfie or image to identify.";
+  showResult = false;
+  keanuFound = false;
+  celebrities = [];
+
+  @ViewChild('fileInput', { static: false })
+  fileInput: ElementRef;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm = this.fb.group({
+      'file': [''],
+    });
+  }
+
+  addFile(event) {
+    this.showResult = false;
+    this.celebrities = [];
+    this.resultMessage = "Looking for Keanu..."
+    const { target: { files } } = event;
+    if (files.length===0) {
+      return;
+    }
+    const file = files[0];
+    Predictions.identify({
+      entities: {
+        source: {
+          file,
+        },
+        celebrityDetection: true
+      }
+    }).then(result => {
+      if (result.entities.length>0) {
+        this.celebrities = [];
+        let keanuFound = result.entities.filter( (entity) => {
+          //@ts-ignore
+          const {metadata: {id} = { } } = entity;
+          if (id) {
+            this.celebrities.push(entity);
+          }
+          return id == "32wO2f3"; // Keanu Reeves
+        })
+        if (keanuFound.length>0) {
+          this.showResult = true;
+          this.keanuFound = true;
+          this.resultMessage = `Congratulations! Keanu found!`;
+        } else {
+          this.showResult = true;
+          this.keanuFound = false;
+          this.resultMessage = `Nope! Keanu is not here! Keep trying!`;
+        }
+      } else {
+        this.showResult = true;
+        this.keanuFound = false;
+        this.resultMessage = `Nope! Keanu is not here! Keep trying!`;
+      }
+    })
+    .catch(err => { 
+      console.log(err);
+      this.resultMessage = "There was an error. Try again later."
+    })
+    .finally(() => {
+      // reset input file
+      this.fileInput.nativeElement.value = "";
+    })
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Auth from '@aws-amplify/auth';
+import Predictions, { AmazonAIPredictionsProvider, InterpretTextCategories } from '@aws-amplify/predictions';
+import amplify from './aws-exports';
+
+Auth.configure(amplify);
+Predictions.configure(amplify);
+Predictions.addPluggable(new AmazonAIPredictionsProvider());
+
 if (environment.production) {
   enableProdMode();
 }
